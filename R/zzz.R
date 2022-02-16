8d388e950e71acd95c2d7c3fe7e716cca230a697
palatej <jean.palate@nbb.be>
2022-02-16 16:01:43 +0100

    v0.1.3

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,9 @@
 
 .onLoad <- function(libname, pkgname) {
+
+  suppressMessages(require(rjd3tramoseats, quietly = T))
+  suppressMessages(require(rjd3x13, quietly = T))
+
   result <- .jpackage(pkgname, lib.loc=libname)
   if (!result) stop("Loading java packages failed")
 }
