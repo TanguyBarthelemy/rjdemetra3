d6d808d9d1e0dbb485108db0a46b17d034683b59
AQLT <alain.quartier@yahoo.fr>
2022-03-21 10:22:05 +0100

    rslt def when jrslt is null

--- R/saitem.R
+++ R/saitem.R
@@ -19,18 +19,22 @@ jsa.read<-function(jsa){
   }
   # ts
   jts<-.jcall(jdef, "Ldemetra/timeseries/Ts;", "getTs")
-  rts<-rjd3toolkit:::ts_jd2r(.jcall(jts, "Ldemetra/timeseries/TsData;", "getData"))
+  rts<-rjd3toolkit::ts_jd2r(.jcall(jts, "Ldemetra/timeseries/TsData;", "getData"))
 
   jspec<-.jcall(jdef, "Ldemetra/sa/SaSpecification;", "activeSpecification")
   if (.jinstanceof(jspec, "demetra/tramoseats/TramoSeatsSpec")){
     spec<-rjd3tramoseats:::jd2r_spec_tramoseats(.jcast(jspec, "demetra/tramoseats/TramoSeatsSpec"))
     if (! is.jnull(jrslt)){
       rslt<-rjd3tramoseats:::tramoseats_rslts(.jcast(jrslt, "jdplus/tramoseats/TramoSeatsResults"))
+    }else{
+      rslt <- NULL
     }
   }else if (.jinstanceof(jspec, "demetra/x13/X13Spec")){
     spec<-rjd3x13:::jd2r_spec_x13(.jcast(jspec, "demetra/x13/X13Spec"))
     if (! is.jnull(jrslt)){
       rslt<-rjd3x13:::x13_rslts(.jcast(jrslt, "jdplus/x13/X13Results"))
+    }else{
+      rslt <- NULL
     }
   }
 
@@ -54,8 +58,8 @@ jsa.results<-function(jsa, items){
   jestimation<-.jcall(jsa, "Ldemetra/sa/SaEstimation;", "getEstimation")
   if (is.jnull(jestimation)) return (NULL)
   jrslt<-.jcall(jestimation, "Ldemetra/information/Explorable;", "getResults")
-  if (is.null(items)) items<-rjd3toolkit:::proc_dictionary2(jrslt)
-  r<-lapply(items, function(t){rjd3toolkit:::proc_data(jrslt, t)})
+  if (is.null(items)) items<-rjd3toolkit::proc_dictionary2(jrslt)
+  r<-lapply(items, function(t){rjd3toolkit::proc_data(jrslt, t)})
   names(r)<-items
   return (r)
 }
